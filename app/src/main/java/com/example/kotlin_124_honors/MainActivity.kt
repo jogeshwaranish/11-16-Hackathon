008e5231ac03c1096c7ef06acf28486c0649187d
Anish <ani.bellamkonda@gmail.com>
2024-11-16 15:04:27 -0600

    updating the code

--- app/src/main/java/com/example/kotlin_124_honors/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_124_honors/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
         setContent {
             Kotlin124honorsTheme {
                 Scaffold(
